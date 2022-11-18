f41a92c901feeb473dbd20cc1a7e7142594edda1
ryou-anubuki <A2oo3o812r>
2022-11-18 14:02:33 +0900

    a

---
+++ CTeam/Project/GameProject/Title/Title.cpp
@@ -0,0 +1,30 @@
+#include"Title.h"
+
+
+
+
+Title::Title() :Base(eType_Scene), m_title_text("C:\\Windows\\Fonts\\msgothic.ttc", 64) {
+
+
+
+
+
+
+
+
+
+
+
+}
+
+Title::~Title()
+{
+}
+
+void Title::Update()
+{
+}
+
+void Title::Draw()
+{
+}
--- CTeam/Project/GameProject/Title/Title.h
+++ CTeam/Project/GameProject/Title/Title.h
@@ -1,1 +1,19 @@
 #pragma once
+#include"../Base/Base.h"
+
+class Title :public Base {
+private:
+	CImage m_img1;
+	CImage anatora;
+	CImage m_img2;//背景
+	CImage m_time;
+	CImage m_title;//タイトル画像「あなぶきリミックス」
+	CFont m_title_text;
+	bool title_flag;
+public:
+	Title();
+	~Title();
+	void Update();
+	void Draw();
+	static int best_time;
+};
