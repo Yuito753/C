16e792e25a9ca83f3c5a34af6972283d428c7888
ryou-anubuki <A2oo3o812r>
2022-12-05 11:42:21 +0900

    a

--- CTeam/Project/GameProject/Title/Title.cpp
+++ CTeam/Project/GameProject/Title/Title.cpp
@@ -6,8 +6,8 @@
 Title::Title() :Base(eType_Scene)  {
 
 	m_img = COPY_RESOURCE("Title", CImage);
-
-
+	m_title_text = COPY_RESOURCE("PACMAN2", CImage);
+	
 
 
 
@@ -23,8 +23,18 @@ Title::~Title()
 
 void Title::Update()
 {
+	/*if (PUSH(CInput::eButton10)) {
+		SOUND("SE_Button2")->Play();
+		SetKill();
+		Base::KillAll();
+		Base::Add(new Game(false));*/
 }
 
 void Title::Draw()
 {
+
+	m_title_text.SetPos(640-340,360-100);
+	m_title_text.Draw();
+	m_img.SetSize(1920, 1080);
+	m_img.Draw();
 }
