58d849c9a814614d1ecaafbc8c97e94fd342168f
ryou-anubuki <A2oo3o812r>
2023-01-16 09:49:51 +0900

    aa

--- CTeam/Project/GameProject/main.cpp
+++ CTeam/Project/GameProject/main.cpp
@@ -72,6 +72,9 @@ void Init(void)
 	ADD_RESOURCE("Title", CImage::CreateImage("Image/Title.png"));
 	ADD_RESOURCE("pacman", CImage::CreateImage("Image/pacman.png"));
 	ADD_RESOURCE("GameClear", CImage::CreateImage("Image/GameClear.png"));
+	ADD_RESOURCE("PACMAN2", CImage::CreateImage("Image/PACMAN2.png"));
+	ADD_RESOURCE("pac", CImage::CreateImage("Image/pac.png"));
+	ADD_RESOURCE("Title_text", CImage::CreateImage("Image/Title_text.png"));
 	//ÉvÉåÉCÉÑÅ[ÇÃê∂ê¨
 	Base::Add(new Title());
 
